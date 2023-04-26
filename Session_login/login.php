6c6f353e7ab7be12bec20beee4b08e9ea3745a51
R-V-ijay <112931433+R-V-ijay@users.noreply.github.com>
2023-04-26 17:42:18 +0530

    Update login.php

--- Session_login/login.php
+++ Session_login/login.php
@@ -20,12 +20,12 @@
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
                     <input type="email" class="form-control" id="userEmail" name="email"
-                        placeholder="Enter Your Email Id">
+                        placeholder="Enter Your Email Id" required>
                 </div>
                 <div class="mb-3">
                     <label for="userPassword" class="form-label">Password</label>
                     <input type="password" class="form-control" id="userPassword" placeholder="Enter Your Password"
-                        name="password">
+                        name="password" required>
                 </div>
                 <div class="d-flex justify-content-center">
                     <button type="submit" class="btn btn-primary" id="sub">Submit</button>
@@ -35,7 +35,9 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.6.4.js"></script>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+    
     <script src="./script.js"></script>
 </body>
 
